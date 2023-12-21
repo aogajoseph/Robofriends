b0a4a7180a3ff0041b7d13b466bcdef43e9017ad
aogajoseph <aogathesecond@gmail.com>
2023-12-21 11:33:50 +0300

    initial commit

---
+++ src/Searchbox.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Searchbox = () => {
+    return (
+        <div className="p2">
+            <input 
+                className="pa3 ba b--green bg-lighttest-blue" type="search" 
+                placeholder="search robots" 
+            />
+        </div>
+    )
+}
+
+export default Searchbox;
